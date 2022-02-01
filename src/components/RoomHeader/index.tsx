d3e1492fb22dc8a83a32e6b5a7397ead6c00c144
EddyPBR <edvaldojunior1310@gmail.com>
2022-02-01 13:03:12 -0300

    feat(component): room header was added

---
+++ src/components/RoomHeader/index.tsx
@@ -0,0 +1,27 @@
+import { AiOutlineEye } from "react-icons/ai";
+
+interface RoomHeaderProps {
+  playersQuantity: number;
+}
+
+export function RoomHeader({ playersQuantity }: RoomHeaderProps) {
+  return (
+    <section className="w-full h-16 bg-gray-900">
+      <div className="container h-full px-4 py-2 flex items-center justify-between">
+        <strong className="text-lg text-white uppercase">
+          Edvaldo Junior&apos;s room
+        </strong>
+
+        <div>
+          <div className="text-white flex items-center gap-1">
+            <AiOutlineEye size={24} />
+            <span className="text-sm">
+              Online players:
+              <strong className="ml-1">{playersQuantity}</strong>
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
