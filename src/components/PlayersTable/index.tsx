6a59226dbaced16d3798fc83ba710385da310432
EddyPBR <edvaldojunior1310@gmail.com>
2022-02-01 22:16:38 -0300

    feat(component): playersTable was enabled scroll

--- src/components/PlayersTable/index.tsx
+++ src/components/PlayersTable/index.tsx
@@ -14,63 +14,65 @@ interface PlayerTableProps {
 
 export function PlayersTable({ players }: PlayerTableProps) {
   return (
-    <table className="w-full">
-      <thead>
-        <tr>
-          <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-800 text-left text-xs font-semibold text-white uppercase">
-            Position
-          </th>
-          <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-800 text-left text-xs font-semibold text-white uppercase">
-            Player
-          </th>
-          <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-800 text-left text-xs font-semibold text-white uppercase">
-            Points
-          </th>
-          <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-800 text-left text-xs font-semibold text-white uppercase" />
-        </tr>
-      </thead>
-      <tbody>
-        {players.map((player, index) => {
-          return (
-            <tr key={index}>
-              <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-gray-700">
-                {index + 1}
-              </td>
-              <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-gray-700">
-                <div className="flex items-center">
-                  <Image
-                    src={player.image_url ?? ""}
-                    alt={player.username}
-                    width="44"
-                    height="44"
-                    className="rounded-full"
-                  />
-                  <p className="ml-2 text-md">{player.username}</p>
-                </div>
-              </td>
-              <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm font-semibold text-gray-700">
-                {player.points}
-              </td>
-              <td className="px-5 py-5 border-b border-gray-200 bg-white text-gray-700">
-                <div className="flex gap-x-4">
-                  <button
-                    type="button"
-                    className="py-2 px-2 bg-lime-500 rounded cursor:pointer hover:bg-lime-600 transition-colors"
-                  >
-                    <TiPlus size={18} className="text-white" />
-                  </button>
-                  <button
-                    type="button"
-                    className="py-2 px-2 bg-red-500 rounded cursor:pointer hover:bg-red-600 transition-colors"
-                  >
-                    <TiMinus size={18} className="text-white" />
-                  </button>
-                </div>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div className="overflow-auto w-full max-h-[calc(100vh-24rem)] scrollbar-thin scrollbar-thumb-gray-800 scrollbar-track-gray-100 scrollbar-thumb-rounded-full scrollbar-thumb-roun">
+      <table className="w-full">
+        <thead>
+          <tr>
+            <th className="sticky z-10 top-0 px-5 py-3 border-b-2 border-gray-200 bg-gray-800 text-left text-xs font-semibold text-white uppercase">
+              Position
+            </th>
+            <th className="sticky z-10 top-0 px-5 py-3 border-b-2 border-gray-200 bg-gray-800 text-left text-xs font-semibold text-white uppercase">
+              Player
+            </th>
+            <th className="sticky z-10 top-0 px-5 py-3 border-b-2 border-gray-200 bg-gray-800 text-left text-xs font-semibold text-white uppercase">
+              Points
+            </th>
+            <th className="sticky z-10 top-0 px-5 py-3 border-b-2 border-gray-200 bg-gray-800 text-left text-xs font-semibold text-white uppercase" />
+          </tr>
+        </thead>
+        <tbody>
+          {players.map((player, index) => {
+            return (
+              <tr key={index}>
+                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-gray-700">
+                  {index + 1}
+                </td>
+                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-gray-700">
+                  <div className="flex items-center">
+                    <Image
+                      src={player.image_url ?? ""}
+                      alt={player.username}
+                      width="44"
+                      height="44"
+                      className="rounded-full"
+                    />
+                    <p className="ml-2 text-md">{player.username}</p>
+                  </div>
+                </td>
+                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm font-semibold text-gray-700">
+                  {player.points}
+                </td>
+                <td className="px-5 py-5 border-b border-gray-200 bg-white text-gray-700">
+                  <div className="flex gap-x-4">
+                    <button
+                      type="button"
+                      className="py-2 px-2 bg-lime-500 rounded cursor:pointer hover:bg-lime-600 transition-colors"
+                    >
+                      <TiPlus size={18} className="text-white" />
+                    </button>
+                    <button
+                      type="button"
+                      className="py-2 px-2 bg-red-500 rounded cursor:pointer hover:bg-red-600 transition-colors"
+                    >
+                      <TiMinus size={18} className="text-white" />
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
