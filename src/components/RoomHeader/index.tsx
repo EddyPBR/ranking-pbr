5640f5cd23a5e31273f746d8f61c9d969154ac81
EddyPBR <edvaldojunior1310@gmail.com>
2022-02-02 21:48:46 -0300

    refactor(component): roomHeader was responsiveness set

--- src/components/RoomHeader/index.tsx
+++ src/components/RoomHeader/index.tsx
@@ -6,20 +6,18 @@ interface RoomHeaderProps {
 
 export function RoomHeader({ playersQuantity }: RoomHeaderProps) {
   return (
-    <section className="w-full h-16 bg-gray-900 rounded-t">
-      <div className="container h-full px-4 py-2 flex items-center justify-between">
+    <section className="w-full h-24 sm:h-16 bg-gray-900 rounded-t">
+      <div className="container h-full px-4 py-2 flex items-center justify-center sm:justify-between flex-col sm:flex-row gap-2">
         <strong className="text-lg text-white uppercase">
           Edvaldo Junior&apos;s room
         </strong>
 
-        <div>
-          <div className="text-white flex items-center gap-1">
-            <AiOutlineEye size={24} />
-            <span className="text-sm">
-              Online players:
-              <strong className="ml-1">{playersQuantity}</strong>
-            </span>
-          </div>
+        <div className="text-white flex items-center gap-1">
+          <AiOutlineEye size={24} />
+          <span className="text-sm">
+            Online players:
+            <strong className="ml-1">{playersQuantity}</strong>
+          </span>
         </div>
       </div>
     </section>
