e33b95b4b4323623b0bf3d52681d459dc23e3c0e
EddyPBR <edvaldojunior1310@gmail.com>
2022-02-02 22:05:36 -0300

    feat(component): playersTable adding responsiveness styles

--- src/components/PlayersTable/index.tsx
+++ src/components/PlayersTable/index.tsx
@@ -18,27 +18,27 @@ export function PlayersTable({ players }: PlayerTableProps) {
       <table className="w-full">
         <thead>
           <tr>
-            <th className="sticky z-10 top-0 px-5 py-3 border-b-2 border-gray-200 bg-gray-800 text-left text-xs font-semibold text-white uppercase">
+            <th className="sticky z-10 top-0 px-5 py-3 border-b-2 border-gray-200 bg-gray-800 text-center sm:text-left text-xs font-semibold text-white uppercase">
               Position
             </th>
-            <th className="sticky z-10 top-0 px-5 py-3 border-b-2 border-gray-200 bg-gray-800 text-left text-xs font-semibold text-white uppercase">
+            <th className="sticky z-10 top-0 px-5 py-3 border-b-2 border-gray-200 bg-gray-800 text-center sm:text-left text-xs font-semibold text-white uppercase">
               Player
             </th>
-            <th className="sticky z-10 top-0 px-5 py-3 border-b-2 border-gray-200 bg-gray-800 text-left text-xs font-semibold text-white uppercase">
+            <th className="sticky z-10 top-0 px-5 py-3 border-b-2 border-gray-200 bg-gray-800 text-center sm:text-left text-xs font-semibold text-white uppercase">
               Points
             </th>
-            <th className="sticky z-10 top-0 px-5 py-3 border-b-2 border-gray-200 bg-gray-800 text-left text-xs font-semibold text-white uppercase" />
+            <th className="hidden sm:table-cell sticky z-10 top-0 px-5 py-3 border-b-2 border-gray-200 bg-gray-800 text-left text-xs font-semibold text-white uppercase" />
           </tr>
         </thead>
         <tbody>
           {players.map((player, index) => {
             return (
-              <tr key={index}>
-                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-gray-700">
+              <tr key={index} className="bg-white hover:bg-gray-100 transition-colors">
+                <td className="px-5 py-5 border-b border-gray-200 text-sm text-center sm:text-left text-gray-700">
                   {index + 1}
                 </td>
-                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-gray-700">
-                  <div className="flex items-center">
+                <td className="px-5 py-5 border-b border-gray-200 text-sm text-center sm:text-left text-gray-700">
+                  <div className="flex items-center flex-col sm:flex-row">
                     <Image
                       src={player.image_url ?? ""}
                       alt={player.username}
@@ -46,13 +46,13 @@ export function PlayersTable({ players }: PlayerTableProps) {
                       height="44"
                       className="rounded-full"
                     />
-                    <p className="ml-2 text-md">{player.username}</p>
+                    <p className="ml-2 text-md text-center sm:text-left">{player.username}</p>
                   </div>
                 </td>
-                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm font-semibold text-gray-700">
+                <td className="px-5 py-5 border-b border-gray-200 text-sm text-center sm:text-left font-semibold text-gray-700">
                   {player.points}
                 </td>
-                <td className="px-5 py-5 border-b border-gray-200 bg-white text-gray-700">
+                <td className="hidden sm:table-cell px-5 py-5 border-b border-gray-200 text-gray-700">
                   <div className="flex gap-x-4">
                     <button
                       type="button"
