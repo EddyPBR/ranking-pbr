f7969a1098abfca763287312174ad7d02f476027
EddyPBR <edvaldojunior1310@gmail.com>
2022-03-27 15:44:17 -0300

    feat(component): createRoom UX and styles adjusts

--- src/components/CreateRoom/index.tsx
+++ src/components/CreateRoom/index.tsx
@@ -4,27 +4,29 @@ import { UserDropdownMenu } from "@components/UserDropdownMenu";
 
 const CreateRoom: FC = () => {
   return (
-    <div className="flex flex-col gap-6">
-      <UserDropdownMenu />
+    <section className="flex flex-col">
+      <div className="mx-auto mb-4">
+        <UserDropdownMenu />
+      </div>
 
-      <form className="mb-6">
+      <form className="mb-6 w-full">
         <input
           type="text"
           id="room_title"
           name="room_title"
-          placeholder="Room's title"
+          placeholder="Name of your room"
           aria-label="enter the title of your room"
-          className="bg-white border w-full h-12 px-4 mb-4 font-sans rounded border-gray-300 outline-gray-500"
+          className="bg-white border w-full h-12 px-4 mb-5 font-sans rounded border-gray-300 outline-gray-500"
         />
         <button
           type="submit"
           className="bg-red-500 text-white h-12 w-full font-sans rounded hover:bg-red-600 transition-colors"
           aria-label="enter the room"
         >
-          Create room
+          Create a room
         </button>
       </form>
-    </div>
+    </section>
   );
 };
 
