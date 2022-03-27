68981480743f42d9b737c83f266f7aa8c1a8f614
EddyPBR <edvaldojunior1310@gmail.com>
2022-03-27 13:09:47 -0300

    feat: add react-loading-skeleton package and config in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import { Toaster } from "react-hot-toast";
+import { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 import { AuthProvider } from "@contexts/AuthContext";
 import type { AppProps } from "next/app";
@@ -8,7 +10,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <Toaster position="top-right" />
-      <Component {...pageProps} />
+      <SkeletonTheme>
+        <Component {...pageProps} />
+      </SkeletonTheme>
     </AuthProvider>
   );
 }
