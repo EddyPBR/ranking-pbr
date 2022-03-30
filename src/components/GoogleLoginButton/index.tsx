a92f8ffdf85a40d3b9de36e6043b183817f26d27
EddyPBR <edvaldojunior1310@gmail.com>
2022-03-29 22:03:47 -0300

    refactor(component): googleLoginButton was toasts updated

--- src/components/GoogleLoginButton/index.tsx
+++ src/components/GoogleLoginButton/index.tsx
@@ -1,23 +1,24 @@
 import type { FC } from "react";
 import { AiOutlineGoogle } from "react-icons/ai";
 
-import { ErrorToast } from "@components/Toasters";
+import { ErrorToast, SuccessToast } from "@components/Toasts";
 import { useAuth } from "@hooks/useAuth";
 
 const GoogleLoginButton: FC = () => {
   const { signInWithGoogle } = useAuth();
 
-  async function handleSignAndCreateRoom() {
+  async function handleSignWithGoogle() {
     try {
       await signInWithGoogle();
+      SuccessToast({ message: "Welcome to Ranking PBR" });
     } catch (error: any) {
-      ErrorToast({ title: "Failed to Login", message: error.message });
+      ErrorToast({ message: error.message });
     }
   }
 
   return (
     <button
-      onClick={handleSignAndCreateRoom}
+      onClick={handleSignWithGoogle}
       className="bg-red-500 text-white h-12 font-sans rounded hover:bg-red-600 transition-colors font-bold flex items-center px-8 mb-12"
     >
       <AiOutlineGoogle size={28} className="mr-6" />
