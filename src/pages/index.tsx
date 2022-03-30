ae9593ca1525ebf95dea6b7a38c120c8c432f2aa
EddyPBR <edvaldojunior1310@gmail.com>
2022-03-29 23:30:10 -0300

    refactor(page): "/" substitute some code by EnterRoom component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { MdDoubleArrow } from "react-icons/md";
 
 import { CreateRoom } from "@components/CreateRoom";
+import { EnterRoom } from "@components/EnterRoom";
 import { GoogleLoginButton } from "@components/GoogleLoginButton";
 import { SEO } from "@components/SEO";
 import { useAuth } from "@hooks/useAuth";
@@ -55,21 +56,7 @@ const Page: NextPage = () => {
               <hr className="w-6/12 bg-gray-700" />
             </div>
 
-            <input
-              type="text"
-              id="room_id"
-              name="room_id"
-              placeholder="Room code..."
-              aria-label="enter the room code"
-              className="bg-white border h-12 px-4 mb-4 font-sans rounded border-gray-3s00"
-            />
-            <button
-              type="button"
-              className="bg-indigo-600 text-white h-12 font-sans rounded hover:bg-indigo-700 mt-2 transition-colors"
-              aria-label="enter the room"
-            >
-              Enter in a room
-            </button>
+            <EnterRoom />
           </div>
         </article>
       </main>
