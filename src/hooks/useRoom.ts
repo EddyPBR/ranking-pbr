cf9312c8565a3f4bd301fb9a45559014bc2c46c3
EddyPBR <edvaldojunior1310@gmail.com>
2022-04-06 23:45:19 -0300

    feat(hook): useRoom was updated to use consume roomContext

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,79 +1,10 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 
-import { ErrorToast } from "@components/Toasts";
-import { database } from "@services/firebase";
-
-import { useAuth } from "./useAuth";
-
-type RoomState = {
-  authorId: string;
-  title: string;
-};
+import { RoomContext } from "@contexts/RoomContext";
 
 const useRoom = () => {
-  const { user } = useAuth();
-
-  const [roomId, setRoomId] = useState<string | undefined>();
-  const [room, setRoom] = useState<RoomState | undefined>();
-  const [isLoadingRoom, setIsLoadingRoom] = useState<boolean>(true);
-
-  const createRoom = async (title: string) => {
-    if (!user) {
-      throw new Error("Unauthorized, please login!");
-    }
-
-    return await database.ref("rooms").push({ title, authorId: user.id });
-  };
-
-  const getRoom = async (roomId: string) => {
-    if (!user) {
-      throw new Error("Unauthorized, please login!");
-    }
-
-    return await database.ref(`rooms/${roomId}`).get();
-  };
-
-  const changeRoomTitle = async (roomId: string, title: string) => {
-    if (!user || !roomId) {
-      throw new Error("Unauthorized, please login!");
-    }
-
-    return await database.ref(`rooms/${roomId}`).update({
-      title,
-    });
-  };
-
-  useEffect(() => {
-    if (!roomId) {
-      return;
-    }
-
-    const getRoomAndSetState = async (roomId: string) => {
-      setIsLoadingRoom(true);
-
-      try {
-        const roomSnapshot = await database.ref(`rooms/${roomId}`).get();
-        const roomData: RoomState = roomSnapshot.val();
-        setRoom(roomData);
-      } catch (error: any) {
-        ErrorToast(error?.message ?? "Unknown error occurs");
-      } finally {
-        setIsLoadingRoom(false);
-      }
-    };
-
-    getRoomAndSetState(roomId);
-  }, [roomId]);
-
-  return {
-    changeRoomTitle,
-    isLoadingRoom,
-    createRoom,
-    getRoom,
-    setRoomId,
-    roomId,
-    room,
-  };
+  const value = useContext(RoomContext);
+  return value;
 };
 
 export { useRoom };
