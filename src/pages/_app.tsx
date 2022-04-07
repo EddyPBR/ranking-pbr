77c141f69b17ce028ab3b2cee61166067cd5dc6e
EddyPBR <edvaldojunior1310@gmail.com>
2022-04-06 23:45:46 -0300

    feat(app): enable RoomProvider in the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,19 @@ import { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 import { AuthProvider } from "@contexts/AuthContext";
+import { RoomProvider } from "@contexts/RoomContext";
 import type { AppProps } from "next/app";
 import "@styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <Toaster position="top-right" />
-      <SkeletonTheme>
-        <Component {...pageProps} />
-      </SkeletonTheme>
+      <RoomProvider>
+        <Toaster position="top-right" />
+        <SkeletonTheme>
+          <Component {...pageProps} />
+        </SkeletonTheme>
+      </RoomProvider>
     </AuthProvider>
   );
 }
