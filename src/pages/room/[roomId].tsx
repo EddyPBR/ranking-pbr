9f45500cda798f154d54a327c00e5dbb739f6880
EddyPBR <edvaldojunior1310@gmail.com>
2022-07-07 21:56:12 -0300

    feat(page): implements players table

--- src/pages/room/[roomId].tsx
+++ src/pages/room/[roomId].tsx
@@ -11,6 +11,15 @@ import {
   useToast,
   Box,
   Container,
+  TableContainer,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Avatar,
+  AvatarBadge,
 } from "@chakra-ui/react";
 import { Profile } from "~components/Profile";
 import { SEO } from "~components/SEO";
@@ -71,6 +80,58 @@ const Page: NextPage = () => {
               </Tooltip>
             </Center>
           </Flex>
+
+          <TableContainer
+            marginTop="3rem"
+            maxHeight="calc(100vh - 224px)"
+            overflowY="auto"
+            overflowX="auto"
+          >
+            <Table>
+              <Thead
+                position="sticky"
+                top="0"
+                backgroundColor="white"
+                zIndex="1"
+              >
+                <Tr>
+                  <Th textAlign="center" width="12%" minWidth="7.5rem">
+                    Position
+                  </Th>
+                  <Th>Player</Th>
+                  <Th width="20%" minWidth="10rem">
+                    Points
+                  </Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {Array(50)
+                  .fill(null)
+                  .map((_, index, arr) => (
+                    <Tr key={index}>
+                      <Td textAlign="center">{index + 1}</Td>
+                      <Td>
+                        <Flex alignItems="center" gap="0.5rem">
+                          <Avatar
+                            size="sm"
+                            name={`A cool player ${index + 1}`}
+                            src="/assets/profile-default.svg"
+                          >
+                            <AvatarBadge
+                              borderColor="papayawhip"
+                              bg="tomato"
+                              boxSize="1em"
+                            />
+                          </Avatar>
+                          A cool player{index + 1}
+                        </Flex>
+                      </Td>
+                      <Td>{arr.length - index}</Td>
+                    </Tr>
+                  ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
         </Container>
       </Box>
     </>
