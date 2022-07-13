43787bec68b09ce6f4d0443a5b40172f8113ddc6
EddyPBR <edvaldojunior1310@gmail.com>
2022-07-12 22:28:32 -0300

    refactor: page.[roomId] table content was turn into a RankingTable.component

--- src/pages/room/[roomId].tsx
+++ src/pages/room/[roomId].tsx
@@ -1,37 +1,15 @@
 import { useEffect, useState } from "react";
-import { BiCopy } from "react-icons/bi";
 
-import {
-  Flex,
-  Spacer,
-  Center,
-  useClipboard,
-  Button,
-  Tooltip,
-  useToast,
-  Box,
-  Container,
-  TableContainer,
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
-  Avatar,
-  AvatarBadge,
-} from "@chakra-ui/react";
-import { Profile } from "~components/Profile";
+import { Box, Container } from "@chakra-ui/react";
+import { Header } from "~components/Header";
+import { RankingTable } from "~components/RankingTable";
 import { SEO } from "~components/SEO";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 
 const Page: NextPage = () => {
   const router = useRouter();
-  const toast = useToast();
-
-  const [roomId, setRoomId] = useState<string | null>(null);
-  const { onCopy } = useClipboard(roomId ?? "");
+  const [roomId, setRoomId] = useState<string>();
 
   useEffect(() => {
     if (!router.isReady) return;
@@ -43,16 +21,6 @@ const Page: NextPage = () => {
     setRoomId(roomId as string);
   }, [router]);
 
-  const handleCopyRoomCode = () => {
-    toast({
-      title: "Room code copied!",
-      status: "info",
-      isClosable: true,
-    });
-
-    onCopy();
-  };
-
   return (
     <>
       <SEO
@@ -61,77 +29,9 @@ const Page: NextPage = () => {
       />
       <Box backgroundColor="primary.500" height="4rem">
         <Container maxW="container.xl">
-          <Flex>
-            <Center>
-              <Profile theme="dark" />
-            </Center>
+          <Header roomId={roomId} isLoading />
 
-            <Spacer />
-
-            <Center>
-              <Tooltip label="Copy room code">
-                <Button
-                  leftIcon={<BiCopy fontSize={18} />}
-                  onClick={handleCopyRoomCode}
-                  backgroundColor="white"
-                >
-                  {roomId}
-                </Button>
-              </Tooltip>
-            </Center>
-          </Flex>
-
-          <TableContainer
-            marginTop="3rem"
-            maxHeight="calc(100vh - 224px)"
-            overflowY="auto"
-            overflowX="auto"
-          >
-            <Table>
-              <Thead
-                position="sticky"
-                top="0"
-                backgroundColor="white"
-                zIndex="1"
-              >
-                <Tr>
-                  <Th textAlign="center" width="12%" minWidth="7.5rem">
-                    Position
-                  </Th>
-                  <Th>Player</Th>
-                  <Th width="20%" minWidth="10rem">
-                    Points
-                  </Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {Array(50)
-                  .fill(null)
-                  .map((_, index, arr) => (
-                    <Tr key={index}>
-                      <Td textAlign="center">{index + 1}</Td>
-                      <Td>
-                        <Flex alignItems="center" gap="0.5rem">
-                          <Avatar
-                            size="sm"
-                            name={`A cool player ${index + 1}`}
-                            src="/assets/profile-default.svg"
-                          >
-                            <AvatarBadge
-                              borderColor="papayawhip"
-                              bg="tomato"
-                              boxSize="1em"
-                            />
-                          </Avatar>
-                          A cool player{index + 1}
-                        </Flex>
-                      </Td>
-                      <Td>{arr.length - index}</Td>
-                    </Tr>
-                  ))}
-              </Tbody>
-            </Table>
-          </TableContainer>
+          <RankingTable />
         </Container>
       </Box>
     </>
