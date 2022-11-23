9f6b381976c5516de67ff69bf4980c3d25292d9a
EddyPBR <edvaldojunior1310@gmail.com>
2022-11-23 00:41:15 -0300

    :sparkles: implements index page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,8 @@ import type { AppProps } from "next/app";
 import { ConfigProvider } from "antd";
 
 import "antd/dist/reset.css";
-import { THEME } from "../styles/theme";
-import { GlobalStyleComponent } from "../styles/GlobalStyleComponent";
+import { THEME } from "~styles/theme";
+import { GlobalStyleComponent } from "~styles/GlobalStyleComponent";
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,89 @@
 import type { NextPage } from "next";
 
-import { Typography, Button } from "antd";
+import NextImage from "next/image";
+
+import {
+  Layout,
+  Row,
+  Col,
+  Card,
+  Typography,
+  Form,
+  Input,
+  Button,
+  message,
+} from "antd";
+import { useState } from "react";
+
+type FormType = {
+  room_code: string;
+};
 
 const Page: NextPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleEnterInRoom = async ({ room_code }: FormType) => {
+    try {
+      setIsLoading(true);
+      // ... create request
+      alert("Código da sala: " + room_code);
+    } catch (error: any) {
+      message.error(error?.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <>
-      <Typography.Title>hello world</Typography.Title>
-      <Button type="primary">Teste</Button>
-    </>
+    <Layout style={{ height: "100vh" }}>
+      <Layout.Content>
+        <Row justify="space-around" align="middle" style={{ height: "100vh" }}>
+          <Col span={8} offset={4}>
+            <NextImage
+              src="/players-game.svg"
+              alt="Ilustração de jogadores torcendo"
+              width={512}
+              height={408}
+            />
+          </Col>
+          <Col span={8} offset={4}>
+            <Card style={{ width: 420 }}>
+              <Typography.Title
+                level={3}
+                style={{ textAlign: "center", marginBottom: "1.5rem" }}
+              >
+                Entrar na sala
+              </Typography.Title>
+
+              <Form<FormType> layout="vertical" onFinish={handleEnterInRoom}>
+                <Form.Item
+                  name="room_code"
+                  label="Código da sala:"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Código da sala é obrigatório",
+                    },
+                  ]}
+                >
+                  <Input placeholder="Ex.: Ajs8agmas8" size="large" />
+                </Form.Item>
+
+                <Button
+                  type="primary"
+                  size="large"
+                  htmlType="submit"
+                  loading={isLoading}
+                  block
+                >
+                  Entrar na sala
+                </Button>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
+      </Layout.Content>
+    </Layout>
   );
 };
 
